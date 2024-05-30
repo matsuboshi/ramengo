0dab3d46adf8c8c9c7027308ffa4f3a08018f97b
matsuboshi <matsuboshi@hotmail.com>
2024-05-30 16:04:07 -0300

    feat: check incoming key and hashing

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/matsuboshi/ramengo
 
 go 1.22.3
+
+require golang.org/x/crypto v0.23.0
--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -1,6 +1,13 @@
 package middleware
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/matsuboshi/ramengo/internal/helpers"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var storedHashedKey = "$2a$10$oX/kWTonHw6YZm6apfplO.XX0ZxRAewn845gBWGMGVp/wMrgDQJwK"
 
 func CorsMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -11,6 +18,19 @@ func CorsMiddleware(next http.HandlerFunc) http.HandlerFunc {
 
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		incomingKey := r.Header.Get("x-api-key")
+
+		if incomingKey == "" {
+			helpers.CustomError(w, "x-api-key header missing", http.StatusForbidden)
+			return
+		}
+
+		err := bcrypt.CompareHashAndPassword([]byte(storedHashedKey), []byte(incomingKey))
+		if err != nil {
+			helpers.CustomError(w, "unauthorized", http.StatusUnauthorized)
 			return
 		}
 
