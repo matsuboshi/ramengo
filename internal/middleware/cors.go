02f4d5199a38de572c1c5eaed7beb5ba97f82683
matsuboshi <matsuboshi@hotmail.com>
2024-05-29 23:18:11 -0300

    fix: allowing new field on header x-api-key

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -6,7 +6,7 @@ func CorsMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "https://tech.redventures.com.br")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, x-api-key")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		next(w, r)
