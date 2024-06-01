b436a25768dc601d961c43487821f69f95546c42
matsuboshi <matsuboshi@hotmail.com>
2024-06-01 12:01:37 -0300

    refactor: concurrent functions in order creation

--- internal/model/order.go
+++ internal/model/order.go
@@ -1,6 +1,9 @@
 package model
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 type Order struct {
 	ID          string `json:"id"`
@@ -9,42 +12,45 @@ type Order struct {
 }
 
 func CreateOrder(secretKey, brothOption, proteinOption string) (Order, error) {
-	brothChan := make(chan string)
-	proteinChan := make(chan string)
-	orderIDChan := make(chan string)
-	errChan := make(chan error)
-	defer close(errChan)
+	brothChan := make(chan string, 1)
+	proteinChan := make(chan string, 1)
+	orderIDChan := make(chan string, 1)
+	errChan := make(chan error, 3)
+	defer close(brothChan)
+	defer close(proteinChan)
+	defer close(orderIDChan)
+
+	var wg sync.WaitGroup
+	wg.Add(3)
 
 	go func() {
-		defer close(brothChan)
+		defer wg.Done()
 		name, err := BrothNameById(brothOption)
 		errChan <- err
 		brothChan <- name
 	}()
 
 	go func() {
-		defer close(proteinChan)
+		defer wg.Done()
 		name, err := ProteinNameById(proteinOption)
 		errChan <- err
 		proteinChan <- name
 	}()
 
 	go func() {
-		defer close(orderIDChan)
+		defer wg.Done()
 		newId, err := GenerateOrderID(secretKey)
 		errChan <- err
 		orderIDChan <- newId
 	}()
 
-	var err error = nil
-	for i := 0; i < 3; i++ {
-		currentError := <-errChan
-		if currentError != nil {
-			err = currentError
+	wg.Wait()
+	close(errChan)
+
+	for err := range errChan {
+		if err != nil {
+			return Order{}, err
 		}
-	}
-	if err != nil {
-		return Order{}, err
 	}
 
 	brothName := <-brothChan
