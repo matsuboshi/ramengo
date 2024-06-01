1ad7aa8c1ac271f9a8df98d88963b86335ab3bec
matsuboshi <matsuboshi@hotmail.com>
2024-06-01 15:32:27 -0300

    feat: instructions on start up

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/matsuboshi/ramengo/internal/handler"
@@ -8,6 +9,11 @@ import (
 )
 
 func main() {
+	fmt.Println("")
+	fmt.Println("Test the endpoint at https://tech.redventures.com.br")
+	fmt.Println("API URL: http://localhost:8080")
+	fmt.Println("API KEY: ZtVdh8XQ2U8pWI2gmZ7f796Vh8GllXoN7mr0djNf")
+
 	http.HandleFunc("/", handler.Greet)
 	http.HandleFunc("/broths", middleware.Cors(handler.ListBroths))
 	http.HandleFunc("/proteins", middleware.Cors(handler.ListProteins))
