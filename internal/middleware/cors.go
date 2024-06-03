cc3f57e4a8e7d449dfddd9f5549dec46888728a6
matsuboshi <matsuboshi@hotmail.com>
2024-06-02 22:34:43 -0300

    refactor: middleware

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 
@@ -8,30 +9,50 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-var storedHashedKey = os.Getenv("STORED_HASHED_KEY")
+const (
+	allowedOrigin  = "https://tech.redventures.com.br"
+	allowedMethods = "GET, POST, OPTIONS"
+	allowedHeaders = "Content-Type, x-api-key"
+	credentials    = "true"
+)
+
+func setCorsHeaders(w http.ResponseWriter) {
+	w.Header().Set("Access-Control-Allow-Origin", allowedOrigin)
+	w.Header().Set("Access-Control-Allow-Methods", allowedMethods)
+	w.Header().Set("Access-Control-Allow-Headers", allowedHeaders)
+	w.Header().Set("Access-Control-Allow-Credentials", credentials)
+}
+
+func authorizeRequest(r *http.Request) error {
+	storedHashedKey := os.Getenv("STORED_HASHED_KEY")
+	if storedHashedKey == "" {
+		return fmt.Errorf("STORED_HASHED_KEY env var not set")
+	}
+
+	incomingKey := r.Header.Get("x-api-key")
+	if incomingKey == "" {
+		return fmt.Errorf("x-api-key header missing")
+	}
+
+	err := bcrypt.CompareHashAndPassword([]byte(storedHashedKey), []byte(incomingKey))
+	if err != nil {
+		return fmt.Errorf("api key does not match")
+	}
+
+	return nil
+}
 
 func Cors(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "https://tech.redventures.com.br")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, x-api-key")
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		setCorsHeaders(w)
 
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusNoContent)
 			return
 		}
 
-		incomingKey := r.Header.Get("x-api-key")
-
-		if incomingKey == "" {
-			customError.WriteMessage(w, "x-api-key header missing", http.StatusForbidden)
-			return
-		}
-
-		err := bcrypt.CompareHashAndPassword([]byte(storedHashedKey), []byte(incomingKey))
-		if err != nil {
-			customError.WriteMessage(w, "unauthorized", http.StatusUnauthorized)
+		if err := authorizeRequest(r); err != nil {
+			customError.WriteMessage(w, err.Error(), http.StatusForbidden)
 			return
 		}
 
